0efea57e92d410aea32952e842005e4b6546c37b
StanleyKaleta <kstanleykale@gmail.com>
2016-12-25 22:18:51 +0100

    started 0.1

--- project/readme/ABOUT_VERSION.md
+++ project/readme/ABOUT_VERSION.md
@@ -1,8 +1,11 @@
 
-# 1.0
-Released: in progress
+# 0.0
+Released: 25.12.2016
 
 	Implemented backend of accounting schema, semantic accounts, procedures and journal.
 	Implemented frontend
-	    - dialogs via manu
+	    - dialogs via menu
 	    - journal, balance and profit tables and thier UI
+
+# 0.1
+Released: in progress
--- src/main/java/org/kaleta/accountant/frontend/Initializer.java
+++ src/main/java/org/kaleta/accountant/frontend/Initializer.java
@@ -19,7 +19,7 @@ import java.util.logging.Logger;
  */
 public class Initializer {
     public static final String NAME = "Accountant";
-    public static final String VERSION = "0.0";
+    public static final String VERSION = "0.1-snapshot";
     public static final String DATA_SOURCE = new File(Initializer.class.getProtectionDomain().getCodeSource().getLocation()
             .getPath()).getParentFile().getPath() + "/" + NAME + "-" + VERSION + "-DATA/";
     public static final Logger LOG = Logger.getLogger("Logger");
