074fbc2171b0ef4b91232f08f45d99c96d7834dc
StanleyKaleta <kstanleykale@gmail.com>
2017-11-24 15:10:45 +0100

    fix: name of general consumption account

--- src/main/java/org/kaleta/accountant/frontend/action/listener/AccountsEditorAccountAction.java
+++ src/main/java/org/kaleta/accountant/frontend/action/listener/AccountsEditorAccountAction.java
@@ -32,20 +32,23 @@ public class AccountsEditorAccountAction extends ActionListener{
     }
 
     public AccountsModel.Account subactionPerformed(String name){
-        String semanticId = String.valueOf(Service.ACCOUNT.getAccountsBySchemaId(getConfiguration().getSelectedYear(),schemaId).size());
+        String year = getConfiguration().getSelectedYear();
+        String semanticId = String.valueOf(Service.ACCOUNT.getAccountsBySchemaId(year,schemaId).size());
 
         AccountsModel.Account createdAccount =
-                Service.ACCOUNT.createAccount(getConfiguration().getSelectedYear(), name, schemaId, semanticId, "");
+                Service.ACCOUNT.createAccount(year, name, schemaId, semanticId, "");
 
         Calendar calendar = Calendar.getInstance();
         String date = String.format("%1$02d%2$02d", calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.MONTH) + 1);
-        Service.TRANSACTIONS.addTransaction(getConfiguration().getSelectedYear(),date, "0",
+        Service.TRANSACTIONS.addTransaction(year,date, "0",
                 createdAccount.getFullId(), Constants.Account.INIT_ACC_ID, Constants.Transaction.OPEN_DESCRIPTION);
 
         if (schemaId.startsWith("1")){
             String consumptionAccId = Service.ACCOUNT.getConsumptionAccountId(schemaId, semanticId);
-            Service.ACCOUNT.createAccount(getConfiguration().getSelectedYear(), Constants.Schema.CONSUMPTION_ACCOUNT_PREFIX + name,
-                    consumptionAccId.split("\\.")[0], consumptionAccId.split("\\.")[1], "");
+            String conAccName = (name.equals(Constants.Account.GENERAL_ACCOUNT_NAME))
+                    ? "General " + Constants.Schema.CONSUMPTION_ACCOUNT_PREFIX + Service.SCHEMA.getGroupName(year, "1", createdAccount.getGroupId())
+                    : Constants.Schema.CONSUMPTION_ACCOUNT_PREFIX + name;
+            Service.ACCOUNT.createAccount(year, conAccName, consumptionAccId.split("\\.")[0], consumptionAccId.split("\\.")[1], "");
         }
 
         getConfiguration().update(Configuration.ACCOUNT_UPDATED);
