85f7f7c77837c474fc7de1c08c0588d1f4ba4059
StanleyKaleta <kstanleykale@gmail.com>
2018-10-13 17:29:32 +0200

    Started development of 1.3

--- accountant-desktop/src/main/java/org/kaleta/accountant/Initializer.java
+++ accountant-desktop/src/main/java/org/kaleta/accountant/Initializer.java
@@ -19,7 +19,7 @@ import java.util.logging.Logger;
  */
 public class Initializer {
     public static final String NAME = "Accountant";
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.3-snapshot";
     public static final Logger LOG = Logger.getLogger("Logger");
     public static int CONTEXT;
 
