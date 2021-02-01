fa481006abbd6bc34f360194b537d67eaa0943da
Stanislav Kaleta <kstanleykale@gmail.com>
2021-02-01 01:11:27 +0100

    started 1.4 devel

--- accountant-desktop/src/main/java/org/kaleta/accountant/Initializer.java
+++ accountant-desktop/src/main/java/org/kaleta/accountant/Initializer.java
@@ -19,7 +19,7 @@ import java.util.logging.Logger;
  */
 public class Initializer {
     public static final String NAME = "Accountant";
-    public static final String VERSION = "1.3";
+    public static final String VERSION = "1.4-snapshot";
     public static final Logger LOG = Logger.getLogger("Logger");
     public static int CONTEXT;
 
@@ -52,7 +52,7 @@ public class Initializer {
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(() -> {
             try {
-                CONTEXT = Constants.Context.PRODUCTION;
+                CONTEXT = Constants.Context.DEVEL;
                 Service.CONFIG.checkResources();
                 Service.CONFIG.checkData();
                 initLogger();
