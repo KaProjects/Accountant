40d649b17a87d65a78d97f316122d89637fda979
Stanislav Kaleta <kstanleykale@gmail.com>
2022-10-22 02:30:57 +0200

    schema account names to lowercase

--- accountant-desktop/src/main/java/org/kaleta/accountant/common/Constants.java
+++ accountant-desktop/src/main/java/org/kaleta/accountant/common/Constants.java
@@ -23,50 +23,50 @@ public class Constants {
 
         public static final String ACCUMULATED_DEP_GROUP_NAME = "Accumulated Depreciation";
         public static final String ACCUMULATED_DEP_GROUP_ID = "9";
-        public static final String ACCUMULATED_DEP_ACCOUNT_PREFIX = "A. D. of ";
+        public static final String ACCUMULATED_DEP_ACCOUNT_PREFIX = "a. d. of ";
 
-        public static final String RECEIVABLES_GROUP_NAME = "Pohladavky";
+        public static final String RECEIVABLES_GROUP_NAME = "Receivables";
         public static final String RECEIVABLES_GROUP_ID = "0";
-        public static final String RECEIVABLES_PERSONAL_ACCOUNT_NAME = "Pohladavky k Osobam";
+        public static final String RECEIVABLES_PERSONAL_ACCOUNT_NAME = "receivables to persons";
         public static final String RECEIVABLES_PERSONAL_ACCOUNT_ID = "0";
-        public static final String RECEIVABLES_INSTITUTION_ACCOUNT_NAME = "Pohladavky k Instituciam";
+        public static final String RECEIVABLES_INSTITUTION_ACCOUNT_NAME = "receivables to institutions";
         public static final String RECEIVABLES_INSTITUTION_ACCOUNT_ID = "1";
-        public static final String RECEIVABLES_COMPANY_ACCOUNT_NAME = "Pohladavky k Spolocnostiam";
+        public static final String RECEIVABLES_COMPANY_ACCOUNT_NAME = "receivables to companies";
         public static final String RECEIVABLES_COMPANY_ACCOUNT_ID = "2";
 
-        public static final String LIABILITIES_GROUP_NAME = "Zavazky";
+        public static final String LIABILITIES_GROUP_NAME = "Liabilities";
         public static final String LIABILITIES_GROUP_ID = "1";
-        public static final String LIABILITIES_PERSONAL_ACCOUNT_NAME = "Zavazky k Osobam";
+        public static final String LIABILITIES_PERSONAL_ACCOUNT_NAME = "liabilities to persons";
         public static final String LIABILITIES_PERSONAL_ACCOUNT_ID = "0";
-        public static final String LIABILITIES_INSTITUTION_ACCOUNT_NAME = "Zavazky k Instituciam";
+        public static final String LIABILITIES_INSTITUTION_ACCOUNT_NAME = "liabilities to institutions";
         public static final String LIABILITIES_INSTITUTION_ACCOUNT_ID = "1";
-        public static final String LIABILITIES_COMPANY_ACCOUNT_NAME = "Zavazky k Spolocnostiam";
+        public static final String LIABILITIES_COMPANY_ACCOUNT_NAME = "liabilities to companies";
         public static final String LIABILITIES_COMPANY_ACCOUNT_ID = "2";
 
         public static final String CAPITAL_GROUP_NAME = "Capital";
         public static final String CAPITAL_GROUP_ID = "0";
-        public static final String CAPITAL_PERSONAL_ACCOUNT_NAME = "Personal Capital";
+        public static final String CAPITAL_PERSONAL_ACCOUNT_NAME = "personal capital";
         public static final String CAPITAL_PERSONAL_ACCOUNT_ID = "0";
 
 
         public static final String DEPRECIATION_GROUP_NAME = "Depreciation";
         public static final String DEPRECIATION_GROUP_ID = "0";
-        public static final String DEPRECIATION_ACCOUNT_PREFIX = "D. of ";
+        public static final String DEPRECIATION_ACCOUNT_PREFIX = "d. of ";
 
         public static final String CONSUMPTION_GROUP_NAME = "Consumption";
         public static final String CONSUMPTION_GROUP_ID = "1";
-        public static final String CONSUMPTION_ACCOUNT_PREFIX = "C. of ";
+        public static final String CONSUMPTION_ACCOUNT_PREFIX = "c. of ";
 
         public static final String BALANCE_GROUP_NAME = "Rozvahove";
         public static final String BALANCE_GROUP_ID = "0";
-        public static final String INIT_BALANCE_ACCOUNT_NAME = "Pociatocny Ucet Rozvazny";
+        public static final String INIT_BALANCE_ACCOUNT_NAME = "pociatocny ucet rozvazny";
         public static final String INIT_BALANCE_ACCOUNT_ID = "0";
-        public static final String CLOSING_BALANCE_ACCOUNT_NAME = "Konecny Ucet Rozvazny";
+        public static final String CLOSING_BALANCE_ACCOUNT_NAME = "konecny ucet rozvazny";
         public static final String CLOSING_BALANCE_ACCOUNT_ID = "1";
 
         public static final String PROFIT_GROUP_NAME = "Vysledkove";
         public static final String PROFIT_GROUP_ID = "1";
-        public static final String PROFIT_STATEMENT_ACCOUNT_NAME = "Vykaz Zisku a Strat";
+        public static final String PROFIT_STATEMENT_ACCOUNT_NAME = "vykaz zisku a strat";
         public static final String PROFIT_STATEMENT_ACCOUNT_ID = "0";
 
         public static final String FIN_CREATION_FULL_ID = "546";
