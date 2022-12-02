0aef41b41611420d23495bee17db1efc3df08e94
Kaleta, Stanislav <stanislav.kaleta@oracle.com>
2022-12-02 20:50:57 +0100

    [desktop] parsed pdf transactions order inverted

--- accountant-desktop/src/main/java/org/kaleta/accountant/backend/manager/PdfParserManager.java
+++ accountant-desktop/src/main/java/org/kaleta/accountant/backend/manager/PdfParserManager.java
@@ -14,6 +14,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class PdfParserManager {
@@ -105,6 +106,8 @@ public class PdfParserManager {
 
             transactions.add(transaction);
         }
+        System.out.println("transactions loaded: " + transactions.size());
+        Collections.reverse(transactions);
         return transactions;
     }
 
